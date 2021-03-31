b3ee6c97a646147e9e91111570d8440932d24074
Haim Natan <Haim.Natan@Enghouse.com>
2021-03-31 11:48:03 +0300

    Added SIP LNP server code

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express        = require('express');
-
+var sip = require('sip');
+var util = require('util');
 
 const app            = express();
 
 const port = 8000;
+
+var serverAddress = "192.168.1.114";
 
 
 const redis     = require('redis');
 var client    = redis.createClient();
 
 client.on("connect", function() {
-	  console.log("You are now connected");
+	  console.log("You are now connected to Redis DB");
 });
 
 app.use(express.json());
@@ -19,3 +22,40 @@ app.use(express.urlencoded({
 }));
 
 require('./app/lnp')(app, client);app.listen(port, () => {  console.log('We are live on ' + port);});
+
+//start of SIP LNP server 
+
+sip.start({
+	address: serverAddress,
+	logger: {
+	  send: function(message, address) { debugger; util.debug("send\n" + util.inspect(message, false, null)); },
+	  recv: function(message, address) { debugger; util.debug("recv\n" + util.inspect(message, false, null)); }
+	}
+  },
+  function (rq) {
+	try {
+	  if(rq.method === 'INVITE') {
+				  sip.send(sip.makeResponse(rq, 100, 'Trying'));
+				  var pnumber = sip.parseUri(rq.uri).user; //extract phone number
+				  console.log(pnumber);
+				  client.get(pnumber, (err, LRN) => {
+					  if (err || LRN == null) {
+						  sip.send(sip.makeResponse(rq, 404, 'Not Found'));
+					  }
+					  else {
+						  var response = sip.makeResponse(rq, 302, 'Moved Temporarily');
+						  console.log("after 302",LRN);
+						  var uri = sip.parseUri(rq.uri);
+						  uri.user = pnumber + ";rn=" + LRN;
+						  response.headers.contact = [{uri: uri}];
+						  sip.send(response);
+  
+					  }
+				  });
+		  }
+  
+	} catch (e) {
+	  sip.send(sip.makeResponse(request, 500, 'Internal Server Error'));
+	  sys.debug('Exception ' + e + ' at ' + e.stack);
+  }
+  });
